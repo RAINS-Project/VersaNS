daa9204d28ca59e1e8037349b191b70d80186b33
Chen Chen <ccmmbupt@gmail.com>
2015-02-18 16:38:07 -0500

    First development commit

--- src/main/java/net/maxgigapop/versans/nps/api/ObjectFactory.java
+++ src/main/java/net/maxgigapop/versans/nps/api/ObjectFactory.java
@@ -24,148 +24,20 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _Teardown_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "teardown");
     private final static QName _TeardownResponse_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "teardownResponse");
     private final static QName _Modify_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "modify");
+    private final static QName _Query_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "query");
     private final static QName _QueryResponse_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "queryResponse");
     private final static QName _ModifyResponse_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "modifyResponse");
-    private final static QName _Setup_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "setup");
     private final static QName _SetupResponse_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "setupResponse");
-    private final static QName _Query_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "query");
+    private final static QName _Setup_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "setup");
+    private final static QName _Teardown_QNAME = new QName("http://maxgigapop.net/versans/nps/api/", "teardown");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: net.maxgigapop.versans.nps.api
      * 
      */
     public ObjectFactory() {
-    }
-
-    /**
-     * Create an instance of {@link QueryContent }
-     * 
-     */
-    public QueryContent createQueryContent() {
-        return new QueryContent();
-    }
-
-    /**
-     * Create an instance of {@link SetupResponseContent }
-     * 
-     */
-    public SetupResponseContent createSetupResponseContent() {
-        return new SetupResponseContent();
-    }
-
-    /**
-     * Create an instance of {@link SetupContent }
-     * 
-     */
-    public SetupContent createSetupContent() {
-        return new SetupContent();
-    }
-
-    /**
-     * Create an instance of {@link ModifyResponseContent }
-     * 
-     */
-    public ModifyResponseContent createModifyResponseContent() {
-        return new ModifyResponseContent();
-    }
-
-    /**
-     * Create an instance of {@link QueryResponseContent }
-     * 
-     */
-    public QueryResponseContent createQueryResponseContent() {
-        return new QueryResponseContent();
-    }
-
-    /**
-     * Create an instance of {@link ModifyContent }
-     * 
-     */
-    public ModifyContent createModifyContent() {
-        return new ModifyContent();
-    }
-
-    /**
-     * Create an instance of {@link TeardownResponseContent }
-     * 
-     */
-    public TeardownResponseContent createTeardownResponseContent() {
-        return new TeardownResponseContent();
-    }
-
-    /**
-     * Create an instance of {@link ServiceExceptionContent }
-     * 
-     */
-    public ServiceExceptionContent createServiceExceptionContent() {
-        return new ServiceExceptionContent();
-    }
-
-    /**
-     * Create an instance of {@link TeardownContent }
-     * 
-     */
-    public TeardownContent createTeardownContent() {
-        return new TeardownContent();
-    }
-
-    /**
-     * Create an instance of {@link Layer2Info }
-     * 
-     */
-    public Layer2Info createLayer2Info() {
-        return new Layer2Info();
-    }
-
-    /**
-     * Create an instance of {@link VlanTag }
-     * 
-     */
-    public VlanTag createVlanTag() {
-        return new VlanTag();
-    }
-
-    /**
-     * Create an instance of {@link ResourceReference }
-     * 
-     */
-    public ResourceReference createResourceReference() {
-        return new ResourceReference();
-    }
-
-    /**
-     * Create an instance of {@link Layer3Info }
-     * 
-     */
-    public Layer3Info createLayer3Info() {
-        return new Layer3Info();
-    }
-
-    /**
-     * Create an instance of {@link ServicePolicy }
-     * 
-     */
-    public ServicePolicy createServicePolicy() {
-        return new ServicePolicy();
-    }
-
-    /**
-     * Create an instance of {@link BgpInfo }
-     * 
-     */
-    public BgpInfo createBgpInfo() {
-        return new BgpInfo();
-    }
-
-    /**
-     * Create an instance of {@link ServiceContract }
-     * 
-     */
-    public ServiceContract createServiceContract() {
-        return new ServiceContract();
     }
 
     /**
@@ -177,12 +49,131 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link TeardownContent }{@code >}}
+     * Create an instance of {@link TeardownContent }
      * 
      */
-    @XmlElementDecl(namespace = "http://maxgigapop.net/versans/nps/api/", name = "teardown")
-    public JAXBElement<TeardownContent> createTeardown(TeardownContent value) {
-        return new JAXBElement<TeardownContent>(_Teardown_QNAME, TeardownContent.class, null, value);
+    public TeardownContent createTeardownContent() {
+        return new TeardownContent();
+    }
+
+    /**
+     * Create an instance of {@link QueryResponseContent }
+     * 
+     */
+    public QueryResponseContent createQueryResponseContent() {
+        return new QueryResponseContent();
+    }
+
+    /**
+     * Create an instance of {@link ResourceReference }
+     * 
+     */
+    public ResourceReference createResourceReference() {
+        return new ResourceReference();
+    }
+
+    /**
+     * Create an instance of {@link Layer2Info }
+     * 
+     */
+    public Layer2Info createLayer2Info() {
+        return new Layer2Info();
+    }
+
+    /**
+     * Create an instance of {@link TeardownResponseContent }
+     * 
+     */
+    public TeardownResponseContent createTeardownResponseContent() {
+        return new TeardownResponseContent();
+    }
+
+    /**
+     * Create an instance of {@link ServicePolicy }
+     * 
+     */
+    public ServicePolicy createServicePolicy() {
+        return new ServicePolicy();
+    }
+
+    /**
+     * Create an instance of {@link ModifyResponseContent }
+     * 
+     */
+    public ModifyResponseContent createModifyResponseContent() {
+        return new ModifyResponseContent();
+    }
+
+    /**
+     * Create an instance of {@link QueryContent }
+     * 
+     */
+    public QueryContent createQueryContent() {
+        return new QueryContent();
+    }
+
+    /**
+     * Create an instance of {@link ServiceExceptionContent }
+     * 
+     */
+    public ServiceExceptionContent createServiceExceptionContent() {
+        return new ServiceExceptionContent();
+    }
+
+    /**
+     * Create an instance of {@link SetupContent }
+     * 
+     */
+    public SetupContent createSetupContent() {
+        return new SetupContent();
+    }
+
+    /**
+     * Create an instance of {@link ServiceContract }
+     * 
+     */
+    public ServiceContract createServiceContract() {
+        return new ServiceContract();
+    }
+
+    /**
+     * Create an instance of {@link BgpInfo }
+     * 
+     */
+    public BgpInfo createBgpInfo() {
+        return new BgpInfo();
+    }
+
+    /**
+     * Create an instance of {@link SetupResponseContent }
+     * 
+     */
+    public SetupResponseContent createSetupResponseContent() {
+        return new SetupResponseContent();
+    }
+
+    /**
+     * Create an instance of {@link ModifyContent }
+     * 
+     */
+    public ModifyContent createModifyContent() {
+        return new ModifyContent();
+    }
+
+    /**
+     * Create an instance of {@link VlanTag }
+     * 
+     */
+    public VlanTag createVlanTag() {
+        return new VlanTag();
+    }
+
+    /**
+     * Create an instance of {@link Layer3Info }
+     * 
+     */
+    public Layer3Info createLayer3Info() {
+        return new Layer3Info();
     }
 
     /**
@@ -204,6 +195,15 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link QueryContent }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://maxgigapop.net/versans/nps/api/", name = "query")
+    public JAXBElement<QueryContent> createQuery(QueryContent value) {
+        return new JAXBElement<QueryContent>(_Query_QNAME, QueryContent.class, null, value);
+    }
+
+    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link QueryResponseContent }{@code >}}
      * 
      */
@@ -222,15 +222,6 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link SetupContent }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://maxgigapop.net/versans/nps/api/", name = "setup")
-    public JAXBElement<SetupContent> createSetup(SetupContent value) {
-        return new JAXBElement<SetupContent>(_Setup_QNAME, SetupContent.class, null, value);
-    }
-
-    /**
      * Create an instance of {@link JAXBElement }{@code <}{@link SetupResponseContent }{@code >}}
      * 
      */
@@ -240,12 +231,21 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link QueryContent }{@code >}}
+     * Create an instance of {@link JAXBElement }{@code <}{@link SetupContent }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "http://maxgigapop.net/versans/nps/api/", name = "query")
-    public JAXBElement<QueryContent> createQuery(QueryContent value) {
-        return new JAXBElement<QueryContent>(_Query_QNAME, QueryContent.class, null, value);
+    @XmlElementDecl(namespace = "http://maxgigapop.net/versans/nps/api/", name = "setup")
+    public JAXBElement<SetupContent> createSetup(SetupContent value) {
+        return new JAXBElement<SetupContent>(_Setup_QNAME, SetupContent.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link TeardownContent }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://maxgigapop.net/versans/nps/api/", name = "teardown")
+    public JAXBElement<TeardownContent> createTeardown(TeardownContent value) {
+        return new JAXBElement<TeardownContent>(_Teardown_QNAME, TeardownContent.class, null, value);
     }
 
 }
